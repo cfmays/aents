7427bde518ce5943719b4087f957b3932becbd6e
Charles Mays <charlesmays@charless-mbp-2.attlocal.net>
2020-02-08 16:21:30 -0500

    procfile web: gunicorn app:create_app

--- run.py
+++ run.py
@@ -7,7 +7,5 @@ from app import create_app
 config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
 
-print "starting..............................."
-
 if __name__ == '__main__':
     app.run()
